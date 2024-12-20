0d1772abaf5ea00fb2bf300e7bbf6ac904c3117d
Kuiko Ihar <coooll81@gmail.com>
2024-12-20 09:22:41 +0100

    task getAverage ended

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -126,8 +126,16 @@ function getStringsLength(arr) {
  *   getAverage([ 1, 10, 100, 1000 ])  => 277,75
  *   getAverage([ 2, 3, 3 ])  => 2,67
  */
-function getAverage(/* arr */) {
-  throw new Error('Not implemented');
+function getAverage(arr) {
+  return Number(
+    (
+      arr.length &&
+      arr.reduce((acc, it) => {
+        return acc + it;
+      }, 0) / arr.length
+    ).toFixed(2)
+  );
+  // throw new Error('Not implemented');
 }
 
 /**
